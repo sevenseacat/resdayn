8a7cd70de749f07d75b134d8736ffd63fabe4846
Rebecca Le <traybaby@gmail.com>
2022-08-09 18:53:58 +0800

    Initial generated Mix app

---
+++ lib/resdayn.ex
@@ -0,0 +1,18 @@
+defmodule Resdayn do
+  @moduledoc """
+  Documentation for `Resdayn`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Resdayn.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/resdayn_test.exs
@@ -0,0 +1,8 @@
+defmodule ResdaynTest do
+  use ExUnit.Case
+  doctest Resdayn
+
+  test "greets the world" do
+    assert Resdayn.hello() == :world
+  end
+end
