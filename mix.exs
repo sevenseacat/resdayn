75107b61309f32ed5d5883f17294ed36083cfcf8
Rebecca Le <traybaby@gmail.com>
2022-08-11 00:16:06 +0800

    Use latest versions of Erlang and Elixir

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Resdayn.MixProject do
     [
       app: :resdayn,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
