a694a4eef20fe3ed3ad21c528ca877df063da19c
Rebecca Le <traybaby@gmail.com>
2025-01-26 17:02:59 +0800

    Only import attribute data once, for the main Morrowind.esm file
    (These are hardcoded in the game but will be imported as part of the process)

--- lib/resdayn/importer/record/attribute.ex
+++ lib/resdayn/importer/record/attribute.ex
@@ -1,17 +1,25 @@
 defmodule Resdayn.Importer.Record.Attribute do
-  def process(_records, _opts) do
+  def process(_records, opts) do
+    # Only import the hardcoded values once, for the main Morrowind.esm fil
+    data =
+      if opts[:filename] == "Morrowind.esm" do
+        [
+          %{id: 0, name: "Strength"},
+          %{id: 1, name: "Intelligence"},
+          %{id: 2, name: "Willpower"},
+          %{id: 3, name: "Agility"},
+          %{id: 4, name: "Speed"},
+          %{id: 5, name: "Endurance"},
+          %{id: 6, name: "Personality"},
+          %{id: 7, name: "Luck"}
+        ]
+      else
+        []
+      end
+
     %{
       resource: Resdayn.Codex.Mechanics.Attribute,
-      data: [
-        %{id: 0, name: "Strength"},
-        %{id: 1, name: "Intelligence"},
-        %{id: 2, name: "Willpower"},
-        %{id: 3, name: "Agility"},
-        %{id: 4, name: "Speed"},
-        %{id: 5, name: "Endurance"},
-        %{id: 6, name: "Personality"},
-        %{id: 7, name: "Luck"}
-      ]
+      data: data
     }
   end
 end
