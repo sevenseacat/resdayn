3f622ef8194f8537ac14e84e1ad0c5263fa5d7f2
Rebecca Le <traybaby@gmail.com>
2025-05-24 13:58:55 +0800

    Ignore one dodgy MiscellaneousItem in Tribunal.esm with no `name`/`icon_filename`

--- lib/resdayn/importer/record/miscellaneous_item.ex
+++ lib/resdayn/importer/record/miscellaneous_item.ex
@@ -7,11 +7,12 @@ defmodule Resdayn.Importer.Record.MiscellaneousItem do
       |> of_type(Resdayn.Parser.Record.MiscellaneousItem)
       |> Enum.map(fn record ->
         record.data
-        |> Map.take([:id, :name, :value, :weight, :script_id])
+        |> Map.take([:id, :name, :value, :weight, :script_id, :icon_filename])
         |> Map.put(:nif_model_filename, record.data.nif_model)
-        |> Map.put(:icon_filename, record.data.icon_filename)
         |> with_flags(:flags, record.flags)
       end)
+      # Ignore one dodgy MiscellaneousItem in Tribunal.esm with no `name`/`icon_filename`
+      |> Enum.reject(&(!Map.get(&1, :name)))
 
     %{
       resource: Resdayn.Codex.Items.MiscellaneousItem,
