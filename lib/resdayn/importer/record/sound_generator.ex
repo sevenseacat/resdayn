d135f1f28a60ab607a36719ba10ea8b8fcb46931
Rebecca Le <traybaby@gmail.com>
2025-05-30 00:28:40 +0800

    Fix one SoundGenerator record missing an id in Bloodmoon.esm

--- lib/resdayn/importer/record/sound_generator.ex
+++ lib/resdayn/importer/record/sound_generator.ex
@@ -4,7 +4,12 @@ defmodule Resdayn.Importer.Record.SoundGenerator do
   def process(records, _opts) do
     records
     |> of_type(Resdayn.Parser.Record.SoundGenerator)
-    |> Enum.map(fn record -> record.data end)
+    |> Enum.map(fn record ->
+      # One Bloodmoon record is missing an ID for some reason
+      Map.update!(record.data, :id, fn val ->
+        val || "BM_horker_0002"
+      end)
+    end)
     |> separate_for_import(Resdayn.Codex.Assets.SoundGenerator)
   end
 end
