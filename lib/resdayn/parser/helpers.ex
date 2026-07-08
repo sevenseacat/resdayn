f02fc4b202b5af76b5877ff0c70d8cdd7da495b9
Rebecca Le <traybaby@gmail.com>
2026-07-08 23:28:40 +0800

    Revert float rounding optimization
    Elixir 1.20 included an optimization on `Float.round/2`, which now makes
    it faster than the previous implementation (130ms vs 150ms)
    
    The previous implementation was much faster on Elixir 1.18 - 160ms vs 940ms

--- lib/resdayn/parser/helpers.ex
+++ lib/resdayn/parser/helpers.ex
@@ -31,9 +31,7 @@ defmodule Resdayn.Parser.Helpers do
   Round a floating-point number to a reasonable number of decimal places.
   """
   def float(float) do
-    float
-    |> :erlang.float_to_binary(decimals: 2)
-    |> :erlang.binary_to_float()
+    Float.round(float, 2)
   end
 
   @doc """
